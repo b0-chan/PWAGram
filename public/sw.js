ce7b1b936a091c1fc65e3890d41ff7d5067a2456
Bo <osipenko226@gmail.com>
2020-02-10 07:24:50 +0200

    add manifest and registeri service worker

---
+++ public/sw.js
@@ -0,0 +1,3 @@
+self.addEventListener('install', (e) => console.log('install', e));
+self.addEventListener('activate', (e) => console.log('activate', e));
+self.addEventListener('fetch', (e) => console.log('fetch', e));
